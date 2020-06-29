a23bddb533141f24c249684b3e8d640f5a5be6a6
karix0202 <kari.klucz@gmail.com>
2020-06-29 17:19:34 +0200

    added one hot encode in utils file

---
+++ utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def one_hot_encode(y):
+    encoded = []
+    if len(y.shape) == 1:
+        for target in y:
+            single = []
+            for nmb in range(0, np.max(y) + 1):
+                if nmb == target:
+                    single.append(1)
+                else:
+                    single.append(0)
+            encoded.append(single)
+        return encoded
+
+    for line in y:
+        single = []
+        max_arg = np.argmax(line)
+        for nmb in range(line.shape[0]):
+            if nmb == max_arg:
+                single.append(1)
+            else:
+                single.append(0)
+        encoded.append(single)
+
+    return encoded
